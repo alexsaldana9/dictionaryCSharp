2c3746c6a37ea02fa332e34e7694b3f8590d048b
Alexandra Saldan <alexsaldana9@gmail.com>
2018-11-14 12:12:46 -0500

    initial commit

---
+++ Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> capitals = new Dictionary<string, string>();
+            //capitals.Add("Alabama", "Montgomery");
+            //capitals.Add("Alaska", "Juneau");
+            //capitals.Add("Arizona", "Phoenix");
+            //capitals.Add("Florida", "Tallahassee");
+
+            //// key = state, value= capital
+
+            //string capitalofAlaska = capitals["Alaska"];
+
+            //Console.WriteLine("The capital of Alaska is  {0} ", capitalofAlaska);
+
+
+            var theTerms = Term.GetTerms();
+            string definitionofDog = theTerms["Dog"].Definition;
+            Console.WriteLine("The definition of dog is {0}", definitionofDog);
+
+
+        }
+    }
+}
---
+++ Dictionary/Term.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Term
+    {
+        public string Definition { get; set; }
+        public List<string> _relatedTerms = new List<string>();
+
+        public Term(string definition, List<string> relatedterms)
+        {
+            Definition = definition;
+            _relatedTerms = relatedterms;
+        }
+
+        public IReadOnlyCollection<string> ReadOnly
+        {
+            get { return _relatedTerms; }
+        }
+
+       
+       
+
+        public static Dictionary<string, Term> GetTerms()
+        {
+
+            var terms = new Dictionary<string, Term>();
+            var dog_related = new List<string> { "mammal", "nice" };
+            var theTerm = new Term("mamifero de cuatro patas", dog_related);
+            terms.Add("Dog", theTerm);
+
+            return terms;
+
+        }
+
+    }
+}
