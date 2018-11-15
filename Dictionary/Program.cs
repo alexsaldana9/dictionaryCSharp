25c33037e8a1691af607c27f2b7365a00fb6b252
Alexandra Saldan <alexsaldana9@gmail.com>
2018-11-15 08:46:43 -0500

    exit function and search word function added to cases

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -8,60 +8,69 @@ namespace Dictionary
 {
     class Program
     {
+
+        public static void search()
+        {
+            Console.WriteLine("Enter word to search: ");
+            string termToSearch = Console.ReadLine();
+            Console.WriteLine("Y: Continue to search - word {0}", termToSearch);
+
+            var theTerms = Term.GetTerms();
+
+            string definitionOfTerm = theTerms[termToSearch].Definition;
+
+            Console.WriteLine("The definition of {0} is {1}", termToSearch, definitionOfTerm);
+
+            List<string> relatedWordsForTerm = theTerms[termToSearch]._relatedTerms;
+
+
+            for (var i = 0; i < relatedWordsForTerm.Count; i++)
+            {
+                Console.WriteLine("Related Term:  {0}", relatedWordsForTerm[i]);
+            }
+        }
         static void Main(string[] args)
         {
 
             // do while () - loop to keep program running
             string openProgram = "Y";
+            
             do
             {
-          
-                Console.WriteLine("Would you like to search for a term? Y or N ");
-                var continueProgram = Console.ReadLine();
+              Console.WriteLine("Would you like to search for a term? Y or N ");
+              var continueProgram = Console.ReadLine();
                 switch (continueProgram)
                 {
                     case "Y" :
                     case "y":
-                        Console.WriteLine("Enter word to search: ");
-                        string termToSearch = Console.ReadLine();
-                        Console.WriteLine("Y: Continue to search - word {0}", termToSearch);
-
-                        var theTerms = Term.GetTerms();
-
-                        //string definitionofDog = theTerms["Dog"].Definition;
-                        string definitionOfTerm = theTerms[termToSearch].Definition;
-
-                        //Console.WriteLine("The definition of dog is {0}", definitionofDog);
-                        Console.WriteLine("he definition of {0} is {1}", termToSearch, definitionOfTerm);
-
-                        //List<string> relatedtermsofDog = theTerms["Dog"]._relatedTerms;
-                        List<string> relatedWordsForTerm = theTerms[termToSearch]._relatedTerms;
-
-                        //for (var i = 0; i < relatedtermsofDog.Count; i++)
-                        //{
-                        //    Console.WriteLine("Related Term:  {0}", relatedtermsofDog[i]);
-                        //}
-
-                        for (var i = 0; i < relatedWordsForTerm.Count; i++)
-                        {
-                            Console.WriteLine("Related Term:  {0}", relatedWordsForTerm[i]);
-                        }
+                        search();
 
                         break;
                     case "N":
                     case "n":
                         Console.WriteLine("Quit  program");
-                        
+                        Environment.Exit(0);
                         break;
                     default:
                         Console.WriteLine("Invalid Selection. Please enter Y or N");
-                        Console.ReadLine();
+                        continueProgram = Console.ReadLine();
+                        if ((openProgram == "Y") || (openProgram == "y"))
+                        {
+                            search();
+                        }
+                        else if ((openProgram == "N") || (openProgram == "n"))
+                        {
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Selection. Please enter Y or N");
+                            continueProgram = Console.ReadLine();
+                        }
                         break;
                 }
-
-
             }
-            while (openProgram != "Y");
+            while ((openProgram == "Y" ) || (openProgram == "y"));
             
             
 
